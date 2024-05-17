e95d9d4e34b831e604b3840a1142086a78b0d5e6
antonyto02 <tonydelro@gmail.com>
2024-05-17 11:45:26 -0600

    si

--- parcial1_2A/5_estructuras_control_ciclicas/while.py
+++ parcial1_2A/5_estructuras_control_ciclicas/while.py
@@ -1,32 +1,35 @@
-#El Ciclo While es una Estrcutura iterativa que se ejecuta X veces siempre y 
-#cuando la condición se cumpla y se seguirá ejecutando tantas veces como se
-#True la condición
+#El Ciclo while es una Estructura iterativa que se ejecuta X veces siempre y cuando la condicion se cumpla y se seguira ejecutando tantas veces como sea True la condicion
 
 # Sintaxis
 #  while condicion:
-#   bloque de instrucciones
+#     bloque de instrucciones
 
-#Ejemplo 1 Crear un programa que imprima en pantalla 5 veces
-#el @
-
-contador=10
-while contador <= 5:
+# Ejemplo 1 Crear un programa que imprima en pantalla 5 veces el @
+contador=1
+while contador<=5:
     print("@")
-    contador +=1
-
-# #Ejemplo 2 Crear un programa que imprima los números del 1 al 5 y los sume y al final imprima la suma
-
-suma = 0
-contador = 1
-
-while contador <= 5:
-    print(contador)
-    suma+=contador
     contador+=1
 
-# #Ejemplo 3 Crear un programa que imprima de multiplicar que el usuario desee
+#Ejemplo 2 Crear un programa que imprima los numeros del 1 al 5 y los sume y al final imprima la suma
 
-# n = int(input("Ingrese el número de la tabla deseada"))
+contador=1
+suma=0
 
-# for i in range (1,11,1):
-#     print(f"{n} X {i} = {n*i}")
+while contador<=5 :
+ print(contador)
+ suma+=contador
+ contador+=1
+
+print(f"La suma es: {suma}")    
+
+# #Ejemplo 3 Crear un programa que imprima la tabla de multiplicar que el usuario desee
+
+tabla=int(input("Ingresa un numero para calcular su tabla de multiplicar: "))
+
+i=1
+multi=0
+
+while i<=10:
+    multi=i*tabla
+    print(f"{tabla} X {i} = {multi}")
+    i+=1
