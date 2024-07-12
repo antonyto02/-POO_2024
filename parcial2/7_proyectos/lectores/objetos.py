c07090ac27a4b5206fa5e61c2cae147a8e501ba1
antonyto02 <tonydelro@gmail.com>
2024-07-12 11:08:57 -0600

    ER Parcial3 CRUD

---
+++ parcial2/7_proyectos/lectores/objetos.py
@@ -0,0 +1,8 @@
+from clases import *
+
+estudiante1 = Estudiantes("Ana Torres Guzman", "Col. Centro 1500 ote", "6181234567", "MECA", "2335678")
+estudiante1.reservar()
+
+
+docente1 = Docentes("Daniel Fuentes Loera", "Fracc. D. Arrieta 1400 nte", "6183335678", "TI", "123")
+docente1.reservar()
